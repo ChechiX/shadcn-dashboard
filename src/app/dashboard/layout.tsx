b78777df6711c2201bac1fb1473925e5d8f0ce99
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-07-30 22:13:24 -0500

    Skeleton and Sheet Component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -19,6 +19,8 @@ const links = [
   { name: 'menu bar', href: 'menu-bar' },
   { name: 'input OTP', href: 'input-otp' },
   { name: 'progress', href: 'progress' },
+  { name: 'sheet', href: 'sheet' },
+  { name: 'skeleton', href: 'skeleton' },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
